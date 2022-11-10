4cc73b92134915e9df283a448a2ea9d8faf07bfa
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-11-10 01:10:28 -0300

    feat: cors

--- src/app.js
+++ src/app.js
@@ -5,8 +5,14 @@ const routes = require("./routes/routes");
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
+// app.use(function (req, res, nex) {
+// 	res.header("Access-Control-Allow-Origin", "*")
+// 	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+// 	res.header("Access-Control-Allow-Credentials", true);
+// 	res.header()
+// })
 
 const port = process.env.PORT || 3333;
 
